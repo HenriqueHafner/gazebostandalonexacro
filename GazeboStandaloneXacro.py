75c81eb5674b92c454673a3998d64bd2130a9b8f
Henrique Hafner Ferreira <henriquehafner@poli.ufrj.br>
2022-07-03 20:37:14 -0300

    refactoring

--- GazeboStandaloneXacro.py
+++ GazeboStandaloneXacro.py
@@ -2,9 +2,16 @@
 """ Created on Thu Jun 30 03:29:25 2022 @author: Henrique Hafner Ferreira """
 
 import xml.etree.ElementTree as et
+import copy
 import json
 
-def fill_xml_obj(xml_obj,data_dic):
+def val_chk(valin):
+    valin = valin.replace(' ','')
+    if valin[-6:] != '_value':
+        return False
+    return True
+
+def fill_xml_obj(xml_obj, data_dic):
     data_dic_unused_keys = list(data_dic.keys())
     tree_root = xml_obj.getroot()
     for element in tree_root.iter():
@@ -15,7 +22,7 @@ def fill_xml_obj(xml_obj,data_dic):
                 element.text = data_dic[element_text_key]
                 if data_dic_unused_keys.count(element_text_key):
                     data_dic_unused_keys.remove(element_text_key)
-            elif element_text_key != '\n':
+            elif element_text_key != '\n' and val_chk(element_text_key):
                 print('No key_text found in data_dic for: ',element_text_key)
 
         keys_attribute = element.attrib.keys()
@@ -27,10 +34,11 @@ def fill_xml_obj(xml_obj,data_dic):
                     element.attrib[curr_key] = data_dic[curr_value]
                     if data_dic_unused_keys.count(curr_value):
                         data_dic_unused_keys.remove(curr_value)
-                elif curr_value != '\n':
+                elif curr_value != '\n' and not val_chk(curr_value):
                     print('No key_attribute found in data_dic for: ',curr_value)
     if len(data_dic_unused_keys)>0:
         print('The following keys were not used to fill any value: ',data_dic_unused_keys)
+        print()
     return xml_obj
 
 def indent(elem, level=0):  
@@ -48,28 +56,36 @@ def indent(elem, level=0):
         if level and (not elem.tail or not elem.tail.strip()):
             elem.tail = i
 
-def load_data(data_path):
-    with open(data_path) as dt:
+def json_data_get(file_path ):
+    with open(file_path) as dt:
         data_stringfied = dt.read()
         data = json.loads(data_stringfied)
     return data
-#data = {'name_value':'name','type_value':'type','parent_value':'parent','child_value':'child','pose_value':'pose','axis_value':'axis','not_used_test':'not_used_test','mass_value':'filled!','inertiatensor_value':'filled!','inertiatensor_value':'filled!','visualname_value':'filled!','meshpath_value':'filled!'} 
 
-model_xml_obj = et.parse('model_template.sdf')
-model_rootelem = model_xml_obj.getroot()
-gazebo_model_data = load_data('gazebo_model_pythondata.json')
-for data_index in len(gazebo_model_data):
-    if data_index == 0:
-        for link_data in gazebo_model_data[data_index]
-        link_template  = et.parse('link_template.sdf')
-        link_xml_obj = fill_xml_obj(link_template,data)
-    
-    joint_template = et.parse('joint_template.sdf')
-    joints_xml_obj = fill_xml_obj(joint_template,data)
+def get_model_json_data(folder_path='' ):
+    file_path = 'gazebo_model_pythondata.json'
+    gazebo_model_data = json_data_get(file_path)
+    return gazebo_model_data
 
+def build_model_sdf_data(model_data):
+    model_xml_obj  = et.parse('model_template.xml')
+    link_template  = et.parse('link_template.xml' )
+    joint_template = et.parse('joint_template.xml')
+    model_rootelem = model_xml_obj.getroot()
+    #build links
+    links_data  = model_data[0]
+    for link_values in links_data:
+        link_xml  = copy.deepcopy(link_template)
+        link_xml_filled = fill_xml_obj(link_xml,link_values)
+        model_rootelem.append(link_xml_filled.getroot())
+    #build joints
+    joint_data = model_data[1]
+    for joint_values in joint_data:
+        joint_xml = copy.deepcopy(joint_template)
+        joints_xml_filled = fill_xml_obj(joint_xml,joint_values)
+        model_rootelem.append(joints_xml_filled.getroot())
+    #build model section
+    indent(model_rootelem)
+    model_xml_obj.write('model_links_joint.xml')
+    return True
 
-
-model_rootelem.append(link_xml_obj.getroot())
-model_rootelem.append(joints_xml_obj.getroot())
-indent(model_rootelem)
-model_xml_obj.write('model-links-joint.sdf')
